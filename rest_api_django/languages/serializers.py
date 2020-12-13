dd99555bc3535b05500037079ac7d6d45c13d35f
Prabodha Jayawardena <pmrjayawardena@gmail.com>
2020-12-13 19:20:47 +0530

    initial commit

---
+++ rest_api_django/languages/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Language
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ("id", "name", "paradigm")

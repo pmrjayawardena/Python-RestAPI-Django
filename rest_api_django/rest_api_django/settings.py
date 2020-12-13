06d0676ac940ec26bfd682447643c0141b6885cd
Prabodha Jayawardena <pmrjayawardena@gmail.com>
2020-12-13 19:21:28 +0530

    changed secret

--- rest_api_django/rest_api_django/settings.py
+++ rest_api_django/rest_api_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "%$*-x3g^$(_n($-$+t9q8bus8_z8gc*0bnkwctr5@cuayp_qz8"
+SECRET_KEY = "%$*-x3g^$(_asasast9q8bus8_z8gc*0bnkwctr5@cuayp_qz8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
